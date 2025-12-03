e926f7fb483f221b9220d60fd75726a02cd4ec56
Sriram1398 <sriramempire98@gmail.com>
2025-12-03 23:23:25 +0530

    initialize Angular application with basic structure and routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import {App} from './app';
+import {Login} from './login/login';
+
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path:'login',component:Login
+  }
+
+];
